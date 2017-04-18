75ee7c8f3b604b5d935ff19c0a2e40f70d3e51e1
charles kelley <charlesckelley>
2017-04-18 02:09:19 -0500

    Move to android has started

--- app/src/main/java/com/andriod/lennyface/monstamath/MathLogic.java
+++ app/src/main/java/com/andriod/lennyface/monstamath/MathLogic.java
@@ -5,10 +5,63 @@ import java.util.Random;
 
 public class MathLogic {
 
-    Random playerNum = new Random();
-    Random monsterNum = new Random();
+    Random rand = new Random();
 
-    int userAnswer;
-    int correctAnswer;
-    
+    private int userAnswer;
+    private int correctAnswer;
+    private int playerNum;
+    private int monsterNum;
+
+    private String type;
+
+    public MathLogic(String math) {
+        type = math;
+    }
+
+    public void createPlayerNum()
+    {
+        playerNum = rand.nextInt(10);
+    }
+
+    public void createMonsterNum()
+    {
+        monsterNum = rand.nextInt(10);
+    }
+
+    public int mathSwitch(String type)
+    {
+        switch(type)
+        {
+            case "addition":
+                correctAnswer = playerNum + monsterNum;
+                break;
+
+            case "subtraction":
+                correctAnswer = playerNum - monsterNum;
+                break;
+
+            case "multiplication":
+                correctAnswer = playerNum * monsterNum;
+                break;
+
+            case "division":
+
+                while(monsterNum == 0)
+                {
+                    createMonsterNum();
+                }
+
+                int isDivisable = playerNum % monsterNum;
+
+                while (isDivisable != 0)
+                {
+                    createPlayerNum();
+                }
+
+                correctAnswer = playerNum / monsterNum;
+                break;
+        }
+        return correctAnswer;
+    }
+
 }
