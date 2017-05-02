6f2e5d3487dee345e583069b32c662c96609be5c
CharlescKelley <charlesckelley@gmail.com>
2017-05-01 23:50:04 -0500

    Update Battle.java

--- app/src/main/java/com/andriod/lennyface/monstamath/Battle.java
+++ app/src/main/java/com/andriod/lennyface/monstamath/Battle.java
@@ -12,12 +12,12 @@ public class Battle {
 
     public int round(int playerHealth, int monsterHealth, int playerDamage, int monsterDamage, boolean playerAttacks)
     {
-
+        // the player/monster health and player/monster damage takin from the GameMain assigned to variables 
         int pHealth = playerHealth;
         int mHealth = monsterHealth;
         int pDamage = playerDamage;
         int mDamage =  monsterDamage;
-
+        // iff statement to determine is the player of the monster lose health and return that lost health
         if(playerAttacks == true)
         {
             mHealth = mHealth - pDamage;
