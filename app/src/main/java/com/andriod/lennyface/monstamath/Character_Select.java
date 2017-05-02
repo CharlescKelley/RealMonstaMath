c8c865abf6d432edc3ce89eba15107dded81f9fe
CharlescKelley <charlesckelley@gmail.com>
2017-05-02 00:07:19 -0500

    Update Character_Select.java

--- app/src/main/java/com/andriod/lennyface/monstamath/Character_Select.java
+++ app/src/main/java/com/andriod/lennyface/monstamath/Character_Select.java
@@ -14,20 +14,23 @@ import static com.andriod.lennyface.monstamath.R.layout.activity_main;
 import static com.andriod.lennyface.monstamath.R.layout.character_select;
 
 public class Character_Select extends AppCompatActivity {
-
+    // Array list because when i send the choosen numbers as an intent it likes arraylist better for some reason not sure why
+    // boolean to tell is the start button is pushed
+    // string for the type of the math the palyer chppses to do
     ArrayList<Integer> pickedNums = new ArrayList<>();
     boolean isrunning = false;
     String mathType = "";
 
-
+    // Buttons for the corrispoding layout so they can be assigned to do something when they are clicked
     Button bAllMath;
     ImageButton numZero, numOne, numTwo, numThree, numFour, numFive, numSix, numSeven, numEight, numNine;
     ImageButton additionClass, subtractionClass, multiplicationClass, divisionClass;
     ImageButton characterSelectBack, startGame;
-
+    // game constructor
     GameMain game;
 
-
+       // when the activity is created it will initialize all the buttons, get the window background for the layout, 
+       // and have what all the buttons do when they are clicked
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -178,7 +181,7 @@ public class Character_Select extends AppCompatActivity {
 
     }
 
-
+        // puts the numbers the player picks into the picknums arraylist
     public void setPickedNums(int num)
     {
         int n = 0;
@@ -187,25 +190,28 @@ public class Character_Select extends AppCompatActivity {
             pickedNums.add(num);
             n++;
         }
+        // test to make sure that the numbers are being put into the arraylist
         Log.d("yea","here it is " + num);
     }
-
+    // puts all the numbers in the array list
     public void setAllNumsPicked()
     {
         for(int i = 0; i < 10; i++)
         {
 
             pickedNums.add(i);
+            // test to see if the numbers are actually in the arraylist
             Log.d("yea","here it is " + pickedNums.get(i));
         }
     }
-
+    // sets the math type to what the player wants
     public void pickClass(String localMathClass)
     {
         mathType = localMathClass;
+        // test to see is the math type is correct
         Log.d("yea","here it is " + mathType);
     }
-
+    // sets the boolean run to true, makes an intent that for the game and sends over the arraylist, mathtype, and boolean to GameMain
     public void runGame()
     {
         isrunning = true;
@@ -216,7 +222,7 @@ public class Character_Select extends AppCompatActivity {
         Log.d("character", pickedNums.toString());
         startActivity(gameMain_intent);
     }
-
+       // back button which goes to previous screen
     public void goBack()
     {
         finish();
